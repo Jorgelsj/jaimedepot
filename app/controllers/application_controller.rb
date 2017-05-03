0ea7b53757fb87263c5c2c6e8072948c89a73efe
JAIME-HP <bmx.bob@hotmail.com>
2017-05-02 20:13:46 -0500

    make it better

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-  #before_action :authorize
-  #before_action :set_i18n_locale_from_params
+  before_action :authorize
+  before_action :set_i18n_locale_from_params
 
   # ...
 
